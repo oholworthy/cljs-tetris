6dd21a78b061347e470692e7960e2964464dd00c
James Henderson <james@pcr.me.uk>
2013-10-20 18:31:04 +0100

    Run `lein new splat clojurex-demo`

---
+++ src/cljs/clojurex_demo/cljs/home.cljs
@@ -0,0 +1,9 @@
+(ns clojurex-demo.cljs.home
+  (:require [dommy.core :as d])
+  (:require-macros [dommy.macros :refer [node sel1]]))
+
+(defn watch-hash! [!hash]
+  (add-watch !hash :home-page
+             (fn [_ _ _ hash]
+               (d/replace-contents! (sel1 :#content) (node [:h2 {:style {:margin-top :1em}}
+                                                            "Hello world from ClojureScript!"])))))
