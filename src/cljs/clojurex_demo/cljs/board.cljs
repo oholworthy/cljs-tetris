190916f244d59a5ca3a5a4d628ad7aa65185b40d
James Henderson <james@pcr.me.uk>
2013-10-31 07:59:23 +0000

    Rendering a canvas

---
+++ src/cljs/clojurex_demo/cljs/board.cljs
@@ -0,0 +1,7 @@
+(ns clojurex-demo.cljs.board)
+
+(def block-size 30)
+
+(def canvas-size
+  {:blocks-wide 10
+   :blocks-tall 15})
--- src/cljs/clojurex_demo/cljs/home.cljs
+++ src/cljs/clojurex_demo/cljs/home.cljs
@@ -1,9 +1,17 @@
 (ns clojurex-demo.cljs.home
-  (:require [dommy.core :as d])
+  (:require [dommy.core :as d]
+            [clojurex-demo.cljs.board :as b])
   (:require-macros [dommy.macros :refer [node sel1]]))
+
+(defn canvas-node []
+  (node
+   (let [{:keys [blocks-tall blocks-wide]} b/canvas-size]
+     [:canvas {:height (* b/block-size blocks-tall)
+               :width (* b/block-size blocks-wide)}])))
 
 (defn watch-hash! [!hash]
   (add-watch !hash :home-page
              (fn [_ _ _ hash]
-               (d/replace-contents! (sel1 :#content) (node [:h2 {:style {:margin-top :1em}}
-                                                            "Hello world from ClojureScript!"])))))
+               (d/replace-contents! (sel1 :#content) (node [:div.row {:style {:margin-top "2em"}}
+                                                            [:div.col-md-6
+                                                             (canvas-node)]])))))
