91ab23e02661f0686ad6074ccf52a5a0f0450ee6
James Henderson <james@pcr.me.uk>
2013-10-31 08:31:48 +0000

    Grid REPL code

--- src/cljs/clojurex_demo/cljs/grid.cljs
+++ src/cljs/clojurex_demo/cljs/grid.cljs
@@ -1,6 +1,19 @@
 (ns clojurex-demo.cljs.grid
-  (:require [dommy.core :as d])
+  (:require [dommy.core :as d]
+            [clojurex-demo.cljs.board :as b])
   (:require-macros [dommy.macros :refer [node sel1]]))
 
 (defn render-grid! [$canvas]
   (def $test-canvas $canvas))
+
+(comment
+  (let [$canvas $test-canvas]
+    (let [context (.getContext $canvas "2d")
+          {:keys [blocks-tall blocks-wide]} b/canvas-size]
+      (doseq [i (range blocks-wide)
+              j (range blocks-tall)]
+        (.strokeRect context
+                     (* i b/block-size)
+                     (* j b/block-size)
+                     b/block-size
+                     b/block-size)))))
