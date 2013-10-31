2c39270b04ce66f4de4881be94cf4459ea4adfc5
James Henderson <james@pcr.me.uk>
2013-10-31 08:33:15 +0000

    Moving grid REPL code into 'render-grid!'

--- src/cljs/clojurex_demo/cljs/grid.cljs
+++ src/cljs/clojurex_demo/cljs/grid.cljs
@@ -4,16 +4,12 @@
   (:require-macros [dommy.macros :refer [node sel1]]))
 
 (defn render-grid! [$canvas]
-  (def $test-canvas $canvas))
-
-(comment
-  (let [$canvas $test-canvas]
-    (let [context (.getContext $canvas "2d")
-          {:keys [blocks-tall blocks-wide]} b/canvas-size]
-      (doseq [i (range blocks-wide)
-              j (range blocks-tall)]
-        (.strokeRect context
-                     (* i b/block-size)
-                     (* j b/block-size)
-                     b/block-size
-                     b/block-size)))))
+  (let [context (.getContext $canvas "2d")
+        {:keys [blocks-tall blocks-wide]} b/canvas-size]
+    (doseq [i (range blocks-wide)
+            j (range blocks-tall)]
+      (.strokeRect context
+                   (* i b/block-size)
+                   (* j b/block-size)
+                   b/block-size
+                   b/block-size))))
