505f486f1cc42fe0fec1e443b9c718c7fc62e31e
James Henderson <james@pcr.me.uk>
2013-11-03 17:50:19 +0000

    Binding to the join button

--- src/cljs/clojurex_demo/cljs/multiplayer_widget.cljs
+++ src/cljs/clojurex_demo/cljs/multiplayer_widget.cljs
@@ -1,16 +1,20 @@
 (ns clojurex-demo.cljs.multiplayer-widget
-  (:require [dommy.core :as d])
+  (:require [dommy.core :as d]
+            [clojure.string :as s])
   (:require-macros [dommy.macros :refer [node sel1]]))
 
-(defn player-name-form-node []
-  (node
-   [:div
-    [:form.form-inline {:role "form" :onsubmit "return false;"}
-     [:fieldset
-      [:input.form-control {:type "text"
-                            :placeholder "Your name"
-                            :style {:width "15em"}}]
-      [:button.btn.btn-primary {:style {:margin-left "1em"}} "Join"]]]]))
+(defn player-name-form-node [bind-join!]
+  (let [$name (node
+               [:input.form-control {:type "text"
+                                     :placeholder "Your name"
+                                     :style {:width "15em"}}])]
+    (node
+     [:div
+      [:form.form-inline {:onsubmit "return false;"}
+       [:fieldset
+        $name
+        (doto (node [:button.btn.btn-primary {:style {:margin-left "1em"}} "Join"])
+          (bind-join! $name))]]])))
 
 (def sample-top-scores
   [{:player "Bob" :score 14}
@@ -30,7 +34,7 @@
          [:tr
           [:td player] [:td {:style {:text-align :right}} score]]))]]]))
 
-(defn multiplayer-node []
+(defn multiplayer-node [bind-join!]
   (node
    [:div {:style {:padding "1em"
                   :margin-top "1em"
@@ -38,13 +42,21 @@
                   :border-radius "1em"}}
     [:h4 "Multiplayer:"]
     [:div {:style {:margin "1em"}}
-     (player-name-form-node)]
+     (player-name-form-node bind-join!)]
     [:div {:style {:margin "1em"}}
      (top-scores-node sample-top-scores)]]))
+
+(defn join-binder [commands-ch]
+  (fn bind-join! [$join-button $name]
+    (d/listen! $join-button :click
+               (fn [_]
+                 (let [name (d/value $name)]
+                   (when-not (s/blank? name)
+                     (js/console.log name "joining.")))))))
 
 (defn make-multiplayer-widget [!top-scores !player-name commands-ch]
   (def !test-top-scores !top-scores)
   (def !test-player-name !player-name)
   (def test-commands-ch commands-ch)
 
-  (multiplayer-node))
+  (multiplayer-node (join-binder commands-ch)))
