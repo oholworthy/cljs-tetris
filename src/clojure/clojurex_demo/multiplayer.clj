d7b328ef1afc942f589766b8e0703cd202bcd159
James Henderson <james@pcr.me.uk>
2013-11-03 20:54:53 +0000

    Websocket echo server

---
+++ src/clojure/clojurex_demo/multiplayer.clj
@@ -0,0 +1,11 @@
+(ns clojurex-demo.multiplayer
+  (:require [chord.http-kit :refer [with-channel]]
+            [clojure.core.async :refer [<! >! put! close! go go-loop]]
+            [clojure.tools.reader.edn :as edn]))
+
+(defn user-joined! [req]
+  (with-channel req ws-ch
+    (go-loop []
+      (when-let [{:keys [message]} (<! ws-ch)]
+        (>! ws-ch (str "Hi client! Server received: " message))
+        (recur)))))
